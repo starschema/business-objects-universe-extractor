2954ec1bcd7a31f4881d5e4fb6a8080b2196cedb
Peter Nagy <peter.nagy@starschema.net>
2016-01-15 18:41:09 +0100

    Modifying the start script. No need to edit the file after this, it is using external env vars.

--- BOMetadataExtraction.bat
+++ BOMetadataExtraction.bat
@@ -1,13 +1,7 @@
 @echo off
-
 setlocal
-rem :: BO Home variable. Installed location of BO
-set BO_HOME=C:\Program Files (x86)\SAP BusinessObjects\SAP BusinessObjects Enterprise XI 4.0
 
-rem :: JAVA Home
-set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_65
 set CURRENT_DIR=%cd%
-
 set _RUNJAVA="%JAVA_HOME%/bin/java"
 set _RUNJAVAC="%JAVA_HOME%/bin/javac"
 set _RUNJAR="%JAVA_HOME%/bin/jar"
@@ -15,7 +9,6 @@ set _RUNJAR="%JAVA_HOME%/bin/jar"
 set CLASSES=%CLASSPATH%;%CURRENT_DIR%\lib\*;%CURRENT_DIR%\resources\*;%BO_HOME%\dataAccess\connectionServer\java\*;%BO_HOME%\SL SDK\java\sl_sdk.jar;
 
 set PATH=C:\Program Files (x86)\SAP BusinessObjects\SAP BusinessObjects Enterprise XI 4.0\win32_x86;%PATH%;
-rem::echo %PATH%
 
 rd /S /Q "build"
 md "build\classes"
