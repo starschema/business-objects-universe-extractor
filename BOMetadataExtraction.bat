48fd7263b4543f7fb1a840fa844ae97ac36cb33f
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2017-06-14 15:23:29 +0200

    added fairly recent 3rd party open source jars to the repo

--- BOMetadataExtraction.bat
+++ BOMetadataExtraction.bat
@@ -2,17 +2,17 @@
 setlocal
 
 set CURRENT_DIR=%cd%
-set _RUNJAVA="%JAVA_HOME%/bin/java"
-set _RUNJAVAC="%JAVA_HOME%/bin/javac"
-set _RUNJAR="%JAVA_HOME%/bin/jar"
+set "_RUNJAVA=%JAVA_HOME%/bin/java"
+set "_RUNJAVAC=%JAVA_HOME%/bin/javac"
+set "_RUNJAR=%JAVA_HOME%/bin/jar"
 
-set CLASSES=%CLASSPATH%;%CURRENT_DIR%\lib\*;%CURRENT_DIR%\resources\*;%BO_HOME%\dataAccess\connectionServer\java\*;%BO_HOME%\SL SDK\java\sl_sdk.jar;
+set CLASSES=%CURRENT_DIR%\lib\gson-2.5.jar;%CURRENT_DIR%\lib\log4j-api-2.8.2.jar;%CURRENT_DIR%\lib\log4j-core-2.8.2.jar;%CURRENT_DIR%\resources\*;%BO_HOME%\dataAccess\connectionServer\java\*;%BO_HOME%\SL SDK\java\sl_sdk.jar
 
 set PATH=C:\Program Files (x86)\SAP BusinessObjects\SAP BusinessObjects Enterprise XI 4.0\win32_x86;%PATH%;
 
 rd /S /Q "build"
 md "build\classes"
 
-%_RUNJAVAC% -cp "%CLASSES%" -sourcepath src -d build\classes src\com\bot\botmeta\BOMetaDataExtraction.java
+"%_RUNJAVAC%" -cp "%CLASSES%" -sourcepath src -d build\classes src\com\bot\botmeta\BOMetaDataExtraction.java
 
-%_RUNJAVA% -Dbusinessobjects.connectivity.directory="%BO_HOME%\dataAccess\connectionServer"  -Dlog4j.configurationFile="%CURRENT_DIR%\resources\log4j2.xml" -cp "%CLASSES%;build\classes" com.bot.botmeta.BOMetaDataExtraction %CONFIG_FILE%
+"%_RUNJAVA%" -Dbusinessobjects.connectivity.directory="%BO_HOME%\dataAccess\connectionServer"  -Dlog4j.configurationFile="%CURRENT_DIR%\resources\log4j2.xml" -cp "%CLASSES%;build\classes" com.bot.botmeta.BOMetaDataExtraction %CONFIG_FILE%
